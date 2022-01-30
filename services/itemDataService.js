cb32c807b787c904b103e416d62281204c5c4d81
cblackw <blackwc@bgsu.edu>
2022-01-30 14:28:22 -0500

    New variant item schema

--- services/itemDataService.js
+++ services/itemDataService.js
@@ -5,85 +5,105 @@ angular.module('bumbyApp')
                 title: "Diaper Cover",
                 link: "/items/diapercover",
                 image: "resources/diapercover.png",
-                options: [
-                    { label: "Waist" },
-                    { label: "Body" },
-                    { label: "Leg Cuffs" }
-                ],
-                extras: []
+                variants: [
+                    {
+                        label: "",
+                        options: [
+                            { label: "Waist" },
+                            { label: "Body" },
+                            { label: "Leg Cuffs" }
+                        ],
+                        extras: []
+                    }
+                ]
             },
             {
                 title: "Shorts - Bloomers",
                 link: "/items/bloomers",
                 image: "resources/Bloomers-laying-down-500x500.jpg",
-                options: [
-                    { label: "Waist" },
-                    { label: "Body" },
-                    { label: "Cuffs" }
-                ],
-                extras: []
+                variants: [
+                    {
+                        label: "",
+                        options: [
+                            { label: "Waist" },
+                            { label: "Body" },
+                            { label: "Cuffs" }
+                        ],
+                        extras: []
+                    }
+                ]
             },
             {
                 title: "Pants - Hemmed",
                 link: "/items/pants",
                 image: "resources/Baby-Standard-Pants-Mild-Tapered-Hemmed-Main.png",
-                options: [
-                    { label: "Waist" },
-                    { label: "Legs" }
-                ],
-                extras: []
+                variants: [
+                    {
+                        label: "",
+                        options: [
+                            { label: "Waist" },
+                            { label: "Legs" }
+                        ],
+                        extras: []
+                    }
+                ]
             },
             {
                 title: "Pants - Joggers",
                 link: "/items/pantsjoggers",
                 image: "resources/joggers.png",
-                options: [
-                    { label: "Waist" },
-                    { label: "Legs" },
-                    { label: "Leg Cuffs" }
-                ],
-                extras: []
+                variants: [
+                    {
+                        label: "",
+                        options: [
+                            { label: "Waist" },
+                            { label: "Legs" },
+                            { label: "Leg Cuffs" }
+                        ],
+                        extras: []
+                    }
+                ]
             },
             {
-                title: "Sheepy Hug",
+                title: "Adult Sweater",
                 link: "/items/sweater",
                 image: "resources/Adult-Sheepy.png",
-                options: [
-                    { label: "Body" },
-                    { label: "Hood" },
-                    { label: "Sleeves" }
-                ],
-                extras: [
-                    { label: "Add Sleeve Cuffs" },
-                    { label: "Add Pocket" }
-                ]
-            },
-            {
-                title: "Sweater - Cardigan",
-                link: "/items/sweatercardigan",
-                image: "resources/Contoured-Zipper-Cardigan.png",
-                options: [
-                    { label: "Body" },
-                    { label: "Hood" },
-                    { label: "Sleeves" }
-                ],
-                extras: [
-                    { label: "Add Sleeve Cuffs" },
-                    { label: "Add Pockets" }
-                ]
-            },
-            {
-                title: "Sweater - Quarter Zip",
-                link: "/items/sweaterquarterzip",
-                image: "resources/Quarter-Zip-Open.png",
-                options: [
-                    { label: "Main Body" },
-                    { label: "Hood" },
-                    { label: "Sleeves" }
-                ],
-                extras: [
-                    { label: "Add Sleeve Cuffs" },
-                    { label: "Add Pocket" }
+                variants: [
+                    {
+                        label: "Sheepy Hug",
+                        options: [
+                            { label: "Body" },
+                            { label: "Hood" },
+                            { label: "Sleeves" },
+                            { label: "Pocket" }
+                        ],
+                        extras: [
+                            { label: "Add Sleeve Cuffs" },
+                            { label: "Cuffed Pocket", colorSelector:"none" }
+                        ]
+                    },
+                    {
+                        label: "Quarter Zip",
+                        options: [
+                            { label: "Body" },
+                            { label: "Neck", types: [
+                                "Hood",
+                                "Tall Collar"
+                            ]},
+                            { label: "Sleeves" },
+                            { label: "Zipper", colorSelector:"zipper" }
+                        ],
+                        extras: [
+                            { label: "Add Sleeve Cuffs" },
+                            { label: "Add Bottom Cuff" },
+                            { label: "Add Pockets", types: [
+                                "Rounded Angled",
+                                "Squared Angled",
+                                "Kangaroo",
+                                "Cuffed Kangaroo"
+                            ]}
+                        ]
+                    }
                 ]
             }
         ];
