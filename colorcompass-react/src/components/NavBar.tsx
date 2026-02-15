993abbe8377b63365a10b4908872891d420ed671
Kenji MacMartin <macmartin.kj@gmail.com>
2026-02-14 17:45:47 -0800

    Import images

--- colorcompass-react/src/components/NavBar.tsx
+++ colorcompass-react/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ export function NavBar(): React.JSX.Element {
   return (
     <nav className="nav">
       <Link className="nav-home" to="/">
-        Logo
+        <img src="/resources/LOGO-5-PNG-TRANSPARENT.png" alt="Bumby Wool Logo" />
       </Link>
       <Link className="nav-link" to="/">
         All Items
