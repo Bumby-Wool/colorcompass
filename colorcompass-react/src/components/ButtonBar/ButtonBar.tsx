2542f3378bfed613719ce145f3cd43ae9d2c1d1e
Kenji MacMartin <macmartin.kj@gmail.com>
2026-03-04 23:45:55 -0800

    Add icons to bottom bar

--- colorcompass-react/src/components/ButtonBar/ButtonBar.tsx
+++ colorcompass-react/src/components/ButtonBar/ButtonBar.tsx
@@ -4,11 +4,26 @@ import "./ButtonBar.css";
 export function ButtonBar(): React.JSX.Element {
   return (
     <div className="button-bar">
-      <button className="action-button">Random</button>
-      <button className="action-button">Undo</button>
-      <button className="action-button">Redo</button>
-      <button className="action-button">Clear</button>
-      <button className="action-button">Screenshot</button>
+      <button className="action-button" type="button">
+        <i className="fas fa-random action-button-icon" aria-hidden="true" />
+        <span>Random</span>
+      </button>
+      <button className="action-button" type="button">
+        <i className="fas fa-undo action-button-icon" aria-hidden="true" />
+        <span>Undo</span>
+      </button>
+      <button className="action-button" type="button">
+        <i className="fas fa-redo action-button-icon" aria-hidden="true" />
+        <span>Redo</span>
+      </button>
+      <button className="action-button" type="button">
+        <i className="fas fa-eraser action-button-icon" aria-hidden="true" />
+        <span>Clear</span>
+      </button>
+      <button className="action-button" type="button">
+        <i className="fas fa-camera action-button-icon" aria-hidden="true" />
+        <span>Screenshot</span>
+      </button>
     </div>
   );
 }
